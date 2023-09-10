0fd7a2de330a37fe9c5e93fbf3634e2179617b8f
laopee <144510028+laopee@users.noreply.github.com>
2023-09-09 22:28:56 -0600

    adding main files

---
+++ SAPWoodClass.py
@@ -0,0 +1,35 @@
+# This code file contains major classes in SAPWood analysis
+
+# Earthquake class EQ
+
+# Push-over class Push
+
+# Displacement protocol class Disp
+
+# General loading class Load
+
+# Spring super class Spr
+
+# Spring sub class Spr_Linear
+
+# Spring sub class Spr_Bilinear
+
+# Spring sub class Spr_CUREE
+
+# Spring sub class Spr_EPHM
+
+# Spring sub class Spr_MultiLinear
+
+# Spring sub class Spr_CompOnly
+
+# Spring sub class Spr_TensionOnly
+
+# Model super class Model
+
+# Model sub class Model_SDOF
+
+# Model sub class Model_ShearBld
+
+# Model sub class Model_Biaxial
+
+# Model sub class Model_6DOF
---
+++ SAPWoodMain.py
@@ -0,0 +1,5 @@
+# This is the main program including GUI
+# this contains sub-routines to read EQ and load files into obj
+# sub-routines to load model files into model obj
+# sub routines to do push over or time history analysis or time history analysis
+# GUIs to control analysis and display results
