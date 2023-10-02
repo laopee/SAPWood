079dcdd4cdd7c5c482efc9a23244a6b43c6d5b98
laopee <144510028+laopee@users.noreply.github.com>
2023-10-02 14:37:56 -0600

    update1

--- SAPWoodMain.py
+++ SAPWoodMain.py
@@ -3,3 +3,27 @@
 # sub-routines to load model files into model obj
 # sub routines to do push over or time history analysis or time history analysis
 # GUIs to control analysis and display results
+import SAPWoodClass as SP
+
+import tkinter as tk
+import matplotlib.pyplot as plt
+
+#Global variables
+EQ_current=SP.Earthquake()
+
+#GUIs
+root=tk.Tk()
+root.title=("load EQ file")
+
+button_load=tk.Button(root,text="load", command=load_eq)
+button_load.pack()
+
+root.mainloop()
+
+#Button functions
+def load_eq():
+    filename=tk.filedialog.askopenfilename()
+    EQ_current.LoadEQ(filename)
+    plt.plot(EQ_current.t,EQ_current.Ax)
+    plt.show()
+    
